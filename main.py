ee0ef0c531feccfcb55c7a2d56002a7776bc428c
Munyaradzi Muchenje <muchenje.munyaradzi@gmail.com>
2024-04-17 10:35:08 +0200

    added changes

---
+++ main.py
@@ -0,0 +1,12 @@
+from PIL import Image
+import pytesseract
+
+# Load the PNG image
+image_path = "burger.png"
+image = Image.open(image_path)
+
+# Perform OCR
+text = pytesseract.image_to_string(image)
+
+# Print the extracted text
+print(text)
